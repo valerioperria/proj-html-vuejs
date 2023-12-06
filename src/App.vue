23d81deba944aaae03ed5f1acdec4f3382fef0f2
valerioperria <valerio.perria@gmail.com>
2023-12-06 14:38:27 +0100

    feat. section6

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import Section2 from "./components/Section2.vue";
 import Section3 from "./components/Section3.vue";
 import Section4 from "./components/Section4.vue";
 import Section5 from "./components/Section5.vue";
+import Section6 from "./components/Section6.vue";
 export default {
-  components: { AppHeader, Section1, Section2, Section3, Section4, Section5 },
+  components: { AppHeader, Section1, Section2, Section3, Section4, Section5, Section6 },
 }; 
 </script>
 
@@ -17,6 +18,7 @@ export default {
   <Section3 />
   <Section4 />
   <Section5 />
+  <Section6 />
 </template>
 
 <style lang="scss">
---
+++ src/components/Section6.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="wrapper">
+            <h5><strong>NEWSLETTER</strong></h5>
+            <h1><strong class="know">Know</strong><strong>First</strong></h1>
+            <p>Follow closely and receive content about our company and the current market.</p>
+        </div>
+        <div class="subscribe">
+            <input type="text" value="Name">
+            <input type="mail" value="Email">
+            <button>SUBSCRIBE</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
+
+.container {
+    padding: 160px 400px;
+    @include flex(flex, row, base-line, center, nowrap, 0);
+    background-image: url(../assets/img/bg-1.jpg);
+    background-size: cover;
+    background-position: center;
+
+    .wrapper {
+        width: 50%;
+
+        h5 {
+            color: $nex;
+            margin-bottom: 2rem;
+        }
+
+        h1 {
+            font-size: 3rem;
+            margin-bottom: 2rem;
+            color: white;
+
+            .know {
+                background-color: $primaryBg;
+                padding: 0.5rem;
+                margin-right: 0.5rem;
+                border-radius: 6px;
+            }
+        }
+
+        p {
+            color: white;
+        }
+    }
+
+    .subscribe {
+        width: 50%;
+
+        @include flex(flex, column, base-line, baseline, nowrap, 0);
+
+        input {
+            width: 100%;
+            padding: 1rem;
+            margin-bottom: 1rem;
+            background-color: rgba(255, 255, 255, 0.575);
+            border: none;
+            border-radius: 6px
+        }
+
+        button {
+            @include button;
+        }
+    }
+}
+</style>
